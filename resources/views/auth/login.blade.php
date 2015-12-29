4dd5b2dce8a5b3b3efb9fdd0cacaa59d3401d266
watchoficina <watchotficina@gmail.com>
2015-12-29 17:16:14 +0100

    formulario polymer

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,23 +1,89 @@
-<form method="POST" action="/auth/login">
+{{--<form method="POST" action="/auth/login">--}}
 
-    @include('partials.errors')
+    {{--{!! csrf_field() !!}--}}
 
-    {!! csrf_field() !!}
+    {{--@include('partials.errors')--}}
 
-    <div>
-        {{trans('forms.email')}}
-        <input type="email" name="email" >
+    {{--<div>--}}
+        {{--{{trans('forms.email')}}--}}
+        {{--<input type="email" name="email" >--}}
+        {{--value="{{ old('email') }}"--}}
+    {{--</div>--}}
+
+    {{--<div>--}}
+        {{--{{trans('forms.password')}}--}}
+        {{--<input type="password" name="password">--}}
+    {{--</div>--}}
+
+    {{--<div>--}}
+        {{--<button type="submit">--}}
+            {{--{{trans('forms.login')}}--}}
+        {{--</button>--}}
+    {{--</div>--}}
+
+{{--</form>--}}
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel Pruebas Polymer</title>
+    <script src="../bower_components/webcomponentsjs/webcomponents.js"></script>
+    <link rel="import" href="../bower_components/polymer/polymer.html">
+    <link rel="import" href="../bower_components/erp-user-component/erp-user-component.html">
+    <link rel="import" href="../bower_components/erp-pass-component/erp-pass-component.html">
+    <link rel="import" href="../bower_components/paper-toolbar/paper-toolbar.html">
+    <link rel="import" href="../bower_components/paper-icon-button/paper-icon-button.html">
+    <link rel="import" href="../bower_components/iron-icons/iron-icons.html">
+    <link rel="import" href="../bower_components/paper-drawer-panel/paper-drawer-panel.html">
+    <link rel="import" href="../bower_components/paper-fab/paper-fab.html">
+    <link rel="import" href="../bower_components/iron-form/iron-form.html">
+
+    <link rel="import" href="../bower_components/paper-input/paper-input.html">
+    <link rel="import" href="../bower_components/paper-button/paper-button.html">
+    <link rel="import" href="../bower_components/paper-styles/paper-styles.html">
+    <link rel="import" href="../bower_components/paper-styles/erp-pages.html">
+
+</head>
+
+<body>
+
+<paper-toolbar class="main">
+    <paper-icon-button icon="menu"></paper-icon-button>
+    <span title>ERP PRUEBAS</span>
+</paper-toolbar>
+
+<div class="login-page">
+
+    {{--<h4>Login</h4>--}}
+
+    <div class="horizontal-section">
+
+        <form method="post" action="/auth/login">
+
+            {!! csrf_field() !!}
+
+            @include('partials.errors')
+
+            <paper-input name="email" label={!!trans('forms.email')!!} value={!! old("email") !!}></paper-input>
+            <paper-input name="password" type="password" label={!!trans('forms.password')!!}></paper-input>
+            <br>
+
+            <br><br><br>
+            <paper-button raised
+                          onclick="clickHandler(event)">{!! trans('forms.login')!!}</paper-button>
+        </form>
+
     </div>
 
-    <div>
-        {{trans('forms.password')}}
-        <input type="password" name="password">
-    </div>
+</div>
 
-    <div>
-        <button type="submit">
-            {{trans('forms.login')}}
-        </button>
-    </div>
+<script>
+    function clickHandler(event) {
+        Polymer.dom(event).localTarget.parentElement.submit();
+    }
+</script>
 
-</form>
+</body>
+
+</html>
